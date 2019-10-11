f4d4840d709cbe0d029c68ddad7716c0f5d98f93
MagicPatze <florian.pontzen@rwth-aachen.de>
2019-10-11 17:31:40 +0200

    combine timeloop AC PF with DC-OPF

--- timeloop_flexigrid.py
+++ timeloop_flexigrid.py
@@ -13,88 +13,64 @@ from pandapower.control.controller.const_control import ConstControl
 #kann ich jeden Tag über diese Funktion einzeln laufen lassen, indem ich in der rundatei davon vielleicht das mache für alle Tage?
 def timeseries_each_day(output_dir, net, timesteps, d, powChRet, powDisRet, powPVRet, powPlugRet, gridnodes):
 
+    #create new variable, because pandapower only uses time_steps in stead of timesteps
+    time_steps = timesteps
+
     #retrieve data source
     profilesLoad, profilesGen, dsLoad, dsGen = retrieve_data_source(timesteps, d, powChRet, powDisRet, powPVRet, powPlugRet, gridnodes)
 
     #create controllers (to control P values of the load and the gen, which are now combined as positive and negative values in one dataframe)
-    create_controllers(net, dsLoad, dsGen)
+    #create one controller for every node.
+    for n in gridnodes:
+        create_controllers(net, dsLoad, dsGen,n)
 
     #the output writer with the desired results to be stored to files
-    #ow = create_output_writer(net, time_steps, output_dir=output_dir)
+    ow = create_output_writer(net, timesteps, output_dir=output_dir)
 
     #the main time series function
-    #run_timeseries(net, time_steps, output_writer=ow)
+    run_timeseries(net, time_steps, output_writer=ow, continue_on_divergence=True)
 
 def retrieve_data_source(timesteps, d, powChRet, powDisRet, powPVRet, powPlugRet, gridnodes):
 
-    #create new two-dimensional lists for the cumulated gen and load
-    #positive value means gen, negative value equals load
-    powDay = {}
-    #try with just using one genDay, because whole timloop depends on d value
-    """genDayOne = {}
-    genDayTwo = {}
-    genDayThree = {}
-    genDayFour = {}
-    genDayFive = {}
-    genDaySix = {}
-    genDaySeven = {}
-    genDayEight = {}
-    genDayNine = {}
-    genDayTen = {}
-    genDayEleven = {}
-    genDayTwelve = {}"""
-
-    for t in timesteps:
-        for n in gridnodes:
-            powDay[n,t] = powPVRet[n,d,t] - powPlugRet[n,d,t] + powDisRet[(9,11,23)][n,d,t] - powChRet[(9,11,23)][n,d,t]
-
-    #split power in load and gen for Const_Controller
+    #calculate new dicts for load and gens with DC-OPF values
     genDay = {}
     loadDay = {}
-    
     for t in timesteps:
         for n in gridnodes:
-            if powDay[n,t] < 0:
-                loadDay[n,t] = powDay[n,t] * -1
-                genDay[n,t] = 0
-            elif powDay[n,t] >= 0:
-                loadDay[n,t] = 0
-                genDay[n,t] = powDay[n,t]
+            genDay[n,t] = powPVRet[n,d,t] + powDisRet[(9,11,23)][n,d,t]
+            loadDay[n,t] = powPlugRet[n,d,t] + powChRet[(9,11,23)][n,d,t]
 
     #transforming dict into an array to write into a dataframe
     genDayArray = np.array([[genDay[n,t] for t in timesteps] for n in gridnodes])
     loadDayArray = np.array([[loadDay[n,t] for t in timesteps] for n in gridnodes])
 
-
     profilesPreLoad = pd.DataFrame(loadDayArray)
     profilesPreGen = pd.DataFrame(genDayArray)
+
     #transpose DataFrame to fit the standard layout of given DataFrame. Afterwards columns are nodes, rows are timesteps
     profilesLoad = profilesPreLoad.transpose()
     profilesGen = profilesPreGen.transpose()
 
     #split up profiles in gen and load profiles, to properly insert them in 2 const_controllers
-
-
-
     dsLoad = DFData(profilesLoad)
     dsGen = DFData(profilesGen)
 
     return profilesLoad, profilesGen, dsLoad, dsGen
 
-def create_controllers(net, dsLoad, dsGen):
+def create_controllers(net, dsLoad, dsGen, n):
 
-    ConstControl(net, element='load',variable='p_mw',element_index=[np.where(0)],data_source=dsLoad, profile_name=["load1_p"])
-    ConstControl(net, element='sgen', variable='p_mw', element_index=[np.where(0)],data_source=dsGen, profile_name=["sgen1_p"])
-    print("stop")
+    ConstControl(net, element='load',variable='p_mw',element_index=net.load.index,data_source=dsLoad, profile_name=[n])
+    ConstControl(net, element='sgen', variable='p_mw', element_index=net.sgen.index,data_source=dsGen, profile_name=[n])
 
-    #create the output writer. Instead of saving the whole net (which would take a lot of time), we extract only pre defined outputs.
-    #In this case we:
+    """create the output writer. Instead of saving the whole net (which would take a lot of time), we extract only pre defined outputs.
+        In this case we:
 
-     #   save the results ro "../timeseries/tests/outputs"
-      #  write the results to ".xls" excel files. (Possible are: .json , .p, . csv"
-       # log the variables "p_mw" from "res_load", "vm_pu" from "res_bus" and two res_line values.
+        save the results ro "../timeseries/tests/outputs"
+        write the results to ".xls" excel files. (Possible are: .json , .p, . csv"
+        log the variables "p_mw" from "res_load", "vm_pu" from "res_bus" and two res_line values."""
 
-def create_output_writer(net, time_steps, output_dir):
+def create_output_writer(net, timesteps, output_dir):
+    time_steps = timesteps
     ow = OutputWriter(net, time_steps, output_path=output_dir, output_file_type=".xls")
     #these variables are saved to the harddisk after / during the time series loop
     ow.log_variable('res_load', 'p_mw')
@@ -107,6 +83,7 @@ def create_output_writer(net, time_steps, output_dir):
     #execution follows:
 
 def run_timeloop(net, timesteps, days, powChRet, powDisRet, powPVRet, powPlugRet, gridnodes):
+
     for d in days:
         output_dir = os.path.join(tempfile.gettempdir(), "time_series_example" + str(d))
         print("Results can be found in your local temp folder: {}".format(output_dir))
